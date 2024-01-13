9e5a24241030d182a76eb08509edfe51d8a294b5
Indu <wjsdlsdn1158@naver.com>
2024-01-13 23:49:26 +0900

    1978

--- 1978_소수찾기.py
+++ 1978_소수찾기.py
@@ -18,26 +18,3 @@ for i in range(N):
         print('sosu 아님')
         is_sosu = True
 print(cnt)
-
-
-'''
-cnt = 0
-is_sosu = True
-N = int(input())
-j = list(map(int, input().split()))
-for i in range(N):
-    for x in range(2, j[i]):
-        if j[i] % x == 0:
-            print(f'{j[i]}가 {x}로 나눠떨어짐!!')
-            is_sosu = False
-            print('is_sosu = False')
-            break
-    if is_sosu == True:
-        print(f'{j[i]}소수니까 cnt에 1 추가')
-        cnt += 1
-    else:
-        print('sosu 아님')
-        is_sosu = True
-print(cnt)
-
-'''
