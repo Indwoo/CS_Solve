c4e06cce439cb8137263de28e3649ce906f93f0f
Indu <wjsdlsdn1158@naver.com>
2024-01-13 21:40:05 +0900

    1978_미완

--- 1978_소수찾기.py
+++ 1978_소수찾기.py
@@ -1,12 +1,15 @@
 cnt = 0
 N = int(input())
 j = list(map(int, input().split()))
+print(j)
 for i in j:
     print(i)
     for x in range(2, 1001):
         if i % x == 0:
+            print(f'{i}는 {x}로 나눠짐!')
             break
         else:
-            continue
-    cnt+=1
+            print(f'{i}는 소수!')
+            cnt += 1
+            break
 print(cnt)
