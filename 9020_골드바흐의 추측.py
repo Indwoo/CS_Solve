ec07b5d476b76024cc11ebff746ce31db976df17
Indu <wjsdlsdn1158@naver.com>
2024-01-15 14:41:11 +0900

    9020_실패

--- 9020_골드바흐의 추측.py
+++ 9020_골드바흐의 추측.py
@@ -1,21 +1,25 @@
-def isPrime(num):
-    if num == 1:
+import math
+
+def d(N): # 소수 판별 함수
+    if N == 1:
         return False
-    else:
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-N = int(input()) # test case
-
-for _ in range(N): 
-    n = int(input())
-    tmp = n // 2
+    for i in range(2, int(math.sqrt(N))+1):
+        if N % i == 0:
+            return False
+    return True
     
-    while tmp > 0:
-        if isPrime(tmp):
-            if isPrime(n - tmp):
-                print(tmp, n-tmp)
-                break
-        tmp -= 1
+N = int(input()) # 테스트 케이스 수 입력
+
+for _ in range(N):
+    num = int(input()) # 짝수 입력
+    
+    A = num // 2 # 두 수 중 줄어드는 변수
+    B = num // 2 # 두 수 중 늘어나는 변수
+    
+    for _ in range(num // 2):
+        if d(A) and d(B): # 두 수가 소수이면 출력
+            print(A, B)
+            break
+        else: # 소수가 아니면 두 수를 줄이고 늘리기
+            A -= 1
+            B += 1
