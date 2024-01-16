426624a1ee60012e1a44f8de8163bfafce879340
Indu <wjsdlsdn1158@naver.com>
2024-01-16 23:53:22 +0900

    2805

--- 2805_나무 자르기.py
+++ 2805_나무 자르기.py
@@ -1,1 +1,21 @@
-input()
+import sys
+
+N, M = list(map(int, input().split()))
+
+tree = list(map(int, input().split()))
+
+max_tree = max(tree)
+cutted_tree = 0
+
+
+while(M>max_tree):
+    
+
+for i in range(max_tree):
+    for j in tree:
+        cutted_tree += j - max_tree
+        if(cutted_tree < 0):
+            cutted_tree = 0
+            continue
+    
+print(cutted_tree)
