e0b623185e71bb11c07cd08bcd987048bb739b6a
Indu <wjsdlsdn1158@naver.com>
2024-01-24 13:25:04 +0900

    2252

--- Gold/[골드_3]_2252_줄 세우기.py
+++ Gold/[골드_3]_2252_줄 세우기.py
@@ -1,11 +1,39 @@
 import sys
+from collections import deque
 
 N, M = map(int, sys.stdin.readline().split())
-height = [[] for _ in range(M+1)]
+height = [[] for _ in range(N+1)]
 
-for i in range(N):
+for i in range(M):
     A, B = map(int, sys.stdin.readline().split())
-    height[A].append([A, B])
-    height[B].append([B, A])
+    height[A].append([B])
     
-print(height)
+def topological_sort(graph):   
+
+    in_degree = {node: 0 for node in graph}
+
+    for node in graph:
+        for neighbor in graph[node]:
+            in_degree[neighbor] += 1
+
+    queue = deque(node for node in in_degree if in_degree[node] == 0)
+
+    sorted_order = []
+
+    while queue:
+        node = queue.popleft()  
+        sorted_order.append(node)
+
+        for neighbor in graph[node]:
+            in_degree[neighbor] -= 1
+
+            if in_degree[neighbor] == 0:
+                queue.append(neighbor)
+
+    if len(sorted_order) != len(graph):
+        return False
+
+    return sorted_order
+
+# 위상 정렬 결과 출력
+print(topological_sort(height))
