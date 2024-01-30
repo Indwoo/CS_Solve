ab747ec6910ac3ea3af77ab3170c30d988879577
Indu <wjsdlsdn1158@naver.com>
2024-01-30 13:29:13 +0900

    9251

---
+++ 3_Gold/[골드_5]_9251_LCS.py
@@ -0,0 +1,13 @@
+import sys
+
+word1, word2 = sys.stdin.readline().strip(), sys.stdin.readline().strip()
+arr = [[0] * (len(word2)+1) for _ in range(len(word1)+1)]
+
+for i in range(1, len(word1)+1):
+    for j in range(1, len(word2)+1):
+        if word1[i-1] == word2[j-1]:
+            arr[i][j] = arr[i-1][j-1] + 1
+        else:
+            arr[i][j] = max(arr[i-1][j], arr[i][j-1])
+
+print(arr[-1][-1])
