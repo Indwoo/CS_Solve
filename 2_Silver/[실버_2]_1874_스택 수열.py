bde0c04ce7f04b2b1cb1de1ee622f4b912eb71a9
Indu <wjsdlsdn1158@naver.com>
2024-04-04 22:59:55 +0900

    1874

---
+++ 2_Silver/[실버_2]_1874_스택 수열.py
@@ -0,0 +1,28 @@
+import sys
+
+N = int(sys.stdin.readline())
+stk_list = []   # 10 3 6 8 7 5 2 1
+is_stack = []   #
+answer = []
+rsp = 0
+
+for i in range(N):
+    stk_list.append(int(sys.stdin.readline().rstrip()))
+i = 0
+while True:
+    i += 1
+    if i <= stk_list[rsp]:
+        is_stack.append(i)
+        answer.append("+")
+    
+    while len(is_stack) != 0 and is_stack[-1] == stk_list[rsp] and rsp != N:
+        is_stack.pop()
+        rsp+=1
+        answer.append("-")
+    if i == N:
+        break    
+if len(answer) == N*2:
+    for i in answer:
+        print(i , end="\n")
+else:
+    print("NO")
