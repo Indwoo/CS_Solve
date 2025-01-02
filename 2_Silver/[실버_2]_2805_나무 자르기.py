452dc9017ce1df7dcb0936cbec1cbf296310d777
Indu <wjsdlsdn1158@naver.com>
2025-01-02 23:36:58 +0900

    2805

--- 2_Silver/[실버_2]_2805_나무 자르기.py
+++ 2_Silver/[실버_2]_2805_나무 자르기.py
@@ -1,16 +1,15 @@
 N, M = map(int, input().split())
 tree = list(map(int, input().split()))
-start, end = 1, max(tree) #이분탐색 검색 범위 설정
+start, end = 1, max(tree) 
 
-while start <= end: #적절한 벌목 높이를 찾는 알고리즘
+while start <= end:
     mid = (start+end) // 2
     
-    log = 0 #벌목된 나무 총합
+    log = 0 
     for i in tree:
         if i >= mid:
             log += i - mid
     
-    #벌목 높이를 이분탐색
     if log >= M:
         start = mid + 1
     else:
