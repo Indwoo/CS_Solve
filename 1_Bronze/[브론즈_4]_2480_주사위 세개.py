238da91904a01805c7ba6e87f5dc0825dfc2f81b
Indu <wjsdlsdn1158@naver.com>
2025-03-16 00:01:59 +0900

    2480

---
+++ 1_Bronze/[브론즈_4]_2480_주사위 세개.py
@@ -0,0 +1,12 @@
+i, j, k = map(int, input().split())
+
+if i == j == k:
+    print(10000+i*1000)
+elif i == j:
+    print(1000+i*100)
+elif i == k:
+    print(1000+i*100)
+elif j == k:
+    print(1000+j*100)
+else:
+    print(100 + max(i,j,k))
